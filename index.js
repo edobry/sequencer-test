31caaf5b2835dc74d18576ba024964fed17b39c2
Eugene Dobry <edobry@gmail.com>
2015-04-01 10:04:00 -0400

    initial

---
+++ index.js
@@ -0,0 +1,104 @@
+
+/**
+ * @name sequencer-test
+ */
+
+var partialRight = function(f) {
+    var slice = Array.prototype.slice;
+    var args = slice.call(arguments, 1);
+ 
+    return function() {
+        return f.apply(this, slice.call(arguments).concat(args));
+    };
+};
+ 
+var halfStep = Math.pow(2, (1/12));
+var wholeStep = Math.pow(halfStep, 2);
+ 
+var scales = {
+  major: ['w', 'w', 'h', 'w', 'w', 'w', 'h'],
+  minor: ['w', 'h', 'w', 'w', 'h', 'w', 'w'],
+};
+ 
+var stepVal = function(step) {
+  var mappings = {
+    'w': wholeStep,
+    'h': halfStep
+  };
+  return mappings[step];
+};
+ 
+var majorVal = partialRight(stepVal, scales.major);
+var minorVal = partialRight(stepVal, scales.minor);
+ 
+var applySteps = function(signal, steps) {
+  return signal * (steps.length > 0
+                     ?  steps.map(majorVal)
+                             .reduce(function(a, b) { return a*b; })
+                     : 1)
+};
+ 
+var notes = ['a', 'b', 'c', 'd', 'e', 'f', 'g'];
+ 
+var playNote = function(signal, note, scale) {
+  var steps = scale.slice(0, notes.indexOf(note));
+  return (steps.length > 0 ? applySteps(signal, steps) : signal);
+};
+ 
+var shiftOctave = function(signal, times) {
+  for(i = 0; i < times; i++) signal = applySteps(signal, scales.major);
+  return signal;
+};
+ 
+var JaggedArray = function() {
+  var exports = {};
+ 
+  var arr = exports.arr = [];
+ 
+  var push = exports.push = function(index, item) {
+    if(!arr[index]) arr[index] = [];
+    arr[index].push(item);
+  };
+ 
+  var get = exports.get = function(index) {
+    return arr[index] ? arr[index] : [];
+  };
+ 
+  return exports;
+};
+ 
+var Sequencer = (function(length){
+  var exports = {};
+ 
+  var scheduled = exports.scheduled = new JaggedArray();
+ 
+  var schedule = exports.schedule = function(start, end, item) {
+    for(var i = start; i <= end; i++)
+      scheduled.push(i, item);
+    return end - start;
+  };
+ 
+  var tick = exports.tick = function(time) {
+    var step = Math.round(time) % length;
+    var out = scheduled.get(step).reduce(function(a,b) { return a + b; }, 0);
+    //console.log(out);
+    return out;
+  };
+ 
+  return exports;
+})(2);
+ 
+var init = true;
+ 
+export function dsp(t) {
+  var amplitude = .6;
+  var base = 2 * Math.PI / 50 0;
+ 
+  Sequencer.schedule(1, 2, playNote(base, 'a', scales.major));
+  //Sequencer.schedule(3, 6, playNote(base, 'd', scales.major));
+  //Sequencer.schedule(7, 10, playNote(base, 'c', scales.major));
+ 
+  //console.log(Sequencer.tick(t));
+ 
+  return amplitude * Math.sin(Sequencer.tick(t)*base);
+}
